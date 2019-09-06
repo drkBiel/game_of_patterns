9064407f76d1f104304fb435f634879f063e2125
7renan <lima.renan96@gmail.com>
2019-09-06 09:31:25 -0300

    Form Login

---
+++ view/login.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Game Of Partterns</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="meu_estilo/grid.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="grid-container">
+  <div class="barra bg-black">
+    <form class="form-inline navbar-form navbar-right" action="/action_page.php">
+       <div class="form-group">
+       <label for="email">Email</label>
+       <input type="email" class="form-control" id="email">
+       </div>
+  
+   <div class="form-group">
+       <label for="pwd">Senha</label>
+       <input type="password" class="form-control" id="pwd">
+   </div>
+  
+   <!--div class="checkbox">
+      <label><input type="checkbox"> Remember me</label>
+   </div-->
+  
+  <button type="submit" class="btn btn-default">Entrar</button>
+</form>
+
+  </div>
+  <div class="cadastro"></div>
+  <div class="rodape"></div>
+  <div class="logo"></div>
+</div>
+
+<!-- Barra superior de login -->   
+<!--nav class="navbar navbar-inverse">
+    <form class="navbar-form navbar" action="">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="E-mail" name="search">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" placeholder="Senha" name="search">
+      </div>
+      <button type="submit" class="btn btn-default">Entrar</button>
+     </form>
+</nav-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
