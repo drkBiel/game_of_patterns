f4d01d90d2981186d8682080ce302f2812d6aa68
7renan <lima.renan96@gmail.com>
2019-09-07 14:32:24 -0300

    Login

--- view/login.php
+++ view/login.php
@@ -1,77 +1,83 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Game Of Partterns</title>
+<?php  
+ include "header.php";
+?>
 
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="meu_estilo/grid.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="grid-container">
-  <div class="barra bg-black">
-    <form class="form-inline navbar-form navbar-right" action="/action_page.php">
-       <div class="form-group">
-       <label for="email">Email</label>
-       <input type="email" class="form-control" id="email">
-       </div>
-  
-   <div class="form-group">
-       <label for="pwd">Senha</label>
-       <input type="password" class="form-control" id="pwd">
+
+<body> 
+<header class="bg-light">
+  <a href=""><img src="img/logo.jpg" width="160" height="60"  class="position-absolute"></a>
+<ul class="nav justify-content-end form-inline">
+  <div class="nav-item form-group">
+   <label class="mr-sm-2 text-primary">Email:</label>
+   <input class="form-control m-2 mr-sm-2" type="email" name="email">
    </div>
-  
-   <!--div class="checkbox">
-      <label><input type="checkbox"> Remember me</label>
-   </div-->
-  
-  <button type="submit" class="btn btn-default">Entrar</button>
-</form>
+   <div class="nav-item form-group">
+   <label class="text-primary mr-sm-2">Senha:</label>
+   <input class="form-control m-2 mr-sm-2" type="password" name="senha">  
+   </div>
+</ul>
+</header>
+<!-- Logo -->
 
+<div class="row">
+  <div class="col-md-6">
+    <br>
+    <br>
+    <h1 align="center">Game Of Patterns</h1><br>
+
+
+</div>
+<!-- form cadastro -->
+  <div class="col-md-6">
+    <div class="container"><br><br>
+      <h3 align="center">Primeira vez no Game Of Patterns?</h3>
+      <h6 align="center">Cadastre-se, é rápido e fácil</h6><br>
+<form class="row">
+  <div class="form-group col-md-12">
+    <label>Endereço de email:</label>
+    <input type="email" class="form-control col-md-10" id="exampleInputEmail1" placeholder="Email">
   </div>
-  <div class="cadastro"></div>
-  <div class="rodape"></div>
-  <div class="logo"></div>
+  
+  <div class="form-group col-md-12">
+    <label>Senha:</label>
+    <input type="password" class="form-control col-md-10" placeholder="Password">
+  </div>
+
+  <div class="form-group col-md-12">
+    <label>Senha:</label>
+    <input type="password" class="form-control col-md-10" placeholder="Confirme sua senha">
+   </div>
+   
+   <div class="form-group col-md-12">
+     <label>Eu sou:</label><br>
+     <input type="radio" name="professor">
+     <label>Professor</label>
+     <input type="radio" name="aluno">
+     <label>Aluno</label>
+     <br>
+   </div>
+  <div align="center" class="form-group col-md-12">
+    <button type="submit" class="btn btn-dark col-md-6">Inscreva - me</button>
+  </div>
+
+  <div align="center" class="form-group col-md-12">
+    <button type="submit" class="btn btn-primary col-md-6">Entre com o Facebook</button>
+  </div>
+
+  <div align="center" class="form-group col-md-12">
+    <button type="submit" class="btn btn-danger col-md-6">Entre com o Google</button>
+  </div>
+</form>
+</div>
+</div>
 </div>
 
-<!-- Barra superior de login -->   
-<!--nav class="navbar navbar-inverse">
-    <form class="navbar-form navbar" action="">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="E-mail" name="search">
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" placeholder="Senha" name="search">
-      </div>
-      <button type="submit" class="btn btn-default">Entrar</button>
-     </form>
-</nav-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<footer>
+  
+</footer>
 
 </body>
-</html>
+
+<?php 
+   include "foot.php";
+?>
