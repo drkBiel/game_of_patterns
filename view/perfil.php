4d9129e56265f14ab3c3b785dc5d0ccc72a899a2
7renan <lima.renan96@gmail.com>
2019-09-17 19:09:24 -0300

    atua

--- view/perfil.php
+++ view/perfil.php
@@ -2,32 +2,18 @@
   include "header.php";
  ?>
 <body>
-  <header class="bg-gradient">
+<header class="bg-gradient">
+<ul>	
   <a href=""><img src="img/logo.jpg" width="160" height="60"  class="img-fluid"></a>
 </ul>
-</header>	
-<div class="row">
+</header>
+
+<div class="row mt-2">
 <div class="col-md-3 text-center">
 	<img src="img/perfil.png" alt="foto_perfil" class="img-thumbnail"><br>
-	<input type="submit" class="btn btn-outline-warning position-relative m-2" value="Atualizar Perfil" name="">
+	<input type="submit" class="btn btn-outline-info position-relative m-2" value="Atualizar Perfil" name="">
 </div>
 
-<div class="col-md-3 boder border-info">
-	<button type="button" class="btn btn-outline-warning">
-		Posição no Ranking <span class="badge badge-info">1º</span>
-</button>
-</div>
-<div class="col-md-3">
-	<button type="button" class="btn btn-outline-warning">
-		Pontuação Geral <span class="badge badge-info">20 Pts</span>
-</button>
-</div>
-<div class="col-md-3">
-	<button type="button" class="btn btn-outline-warning">
-		Número de Quizzes Respondidos <span class="badge badge-info">2</span>
-	</button>
-</div>
-</div>
 
 
 
@@ -35,6 +21,7 @@
 
 
 </body>
+
  <?php 
    include "foot.php"; 
  ?>
