7e0207d9ae1140a93e6e825f4c25be1e30783d9d
Renan Lima <lima.renan96@gmail.com>
2019-09-23 12:31:32 -0300

    update perfil

--- view/perfil.php
+++ view/perfil.php
@@ -3,7 +3,7 @@
  ?>
 <body>
 <header class="bg-gradient"> 
-  <a href=""><img src="../img/logo.jpg" width="160" height="60"  class="position-absolute"></a>
+  <a href=""><img src="../img/logo.jpg" width="160" height="60"  class="position-absolute img-fluid"></a>
 <ul class="nav justify-content-end form-inline">
   <li class="nav-item">
     <button type="button" class="btn btn-outline-info m-2">
@@ -23,15 +23,20 @@
 
 <div class="row">
    <div class="col-3">
-     <img src="../img/perfil.png" alt="perfil" class="rounded">
+     <img src="../img/perfil.png" alt="perfil" class="rounded img-fluid">
    </div>  
 
    <div class="col-9">
      <h3 class="text-warning">Nome de usuario</h3>
-     <div class="card">
-       
-     </div>
-   </div>
+    <div class="col-4 border border-warning rounded">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="text-info">Pontuação no Ranking</h5>
+        <p><span class="badge badge-light">20</span></p>
+      </div>
+    </div>
+  </div>
+</div> <!-- col-9 -->
 </div> <!-- row -->
 <div class="row">
   <div class="col-3">
@@ -39,9 +44,9 @@
   </div>
   
   <div class="col-9">
-   <h3 class="text-warning">Nome de usuario</h3>
+   <h3 class="text-warning">Minhas Badges</h3>
+  
 
-      
   </div>
 </div>
  <?php 
