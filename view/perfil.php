2589d35a004a7ae2644026e031e2280835baaa4a
Renan Lima <lima.renan96@gmail.com>
2019-09-23 11:34:06 -0300

    Nova tela de perfil

--- view/perfil.php
+++ view/perfil.php
@@ -3,7 +3,7 @@
  ?>
 <body>
 <header class="bg-gradient"> 
-  <a href=""><img src="img/logo.jpg" width="160" height="60"  class="position-absolute"></a>
+  <a href=""><img src="../img/logo.jpg" width="160" height="60"  class="position-absolute"></a>
 <ul class="nav justify-content-end form-inline">
   <li class="nav-item">
     <button type="button" class="btn btn-outline-info m-2">
@@ -20,97 +20,27 @@
   </li>
   </ul>
 </header> 
-<div class="col-md-12">
-<div class="row mt-4">
-<div class="col-md-3">
-	<div class="container text-center">
-	<img src="img/perfil.png">
-	<hr>
-	<h6>Renan Lima - <small><i>IFCE, 8º Semestre</i></small></h6>
-	</div>	
-</div>
-<div class="col-md-9">
-  <div class="row">
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body text-center">
-        <h5 class="card-title"><img src="img/interrogacao.ico"></h5>
-        <button type="button" class="btn btn-outline-warning">
-          Ranking <span class="badge badge-light">4º</span>
-       </button>
-      </div>
-    </div>
-  </div>
-   
-   <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body text-center">
-        <h5 class="card-title"><img src="img/interrogacao.ico"></h5>
-        <button type="button" class="btn btn-outline-warning">
-          Pontuação Geral <span class="badge badge-light">10</span>
-       </button>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body text-center">
-        <h5 class="card-title"><img src="img/interrogacao.ico"></h5>
-        <button type="button" class="btn btn-outline-warning">
-          Quizzes Respondidos <span class="badge badge-light">3</span>
-       </button>
-      </div>
-    </div>
-  </div>
-  <hr>
-  <!--div class="col-sm-4 mt-2">
-    <div class="card">
-      <div class="card-body text-center">
-        <h5 class="card-title"><img src="img/interrogacao.ico"></h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4 mt-2">
-    <div class="card">
-      <div class="card-body text-center">
-        <h5 class="card-title"><img src="img/interrogacao.ico"></h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4 mt-2">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title"><img src="img/interrogacao.ico"></h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div-->
- 
-  
-</div>
-</div>
-</div>
-</div>
 
+<div class="row">
+   <div class="col-3">
+     <img src="../img/perfil.png" alt="perfil" class="rounded">
+   </div>  
 
+   <div class="col-9">
+     <h3 class="text-warning">Nome de usuario</h3>
+     <div class="card">
+       
+     </div>
+   </div>
+</div> <!-- row -->
+<div class="row">
+  <div class="col-3">
     
-<!--footer class="card text-center mt-2">
-
-  <div class="card-body">
-  	<h5 class="card-title text-warning">Aceitamos Sugestões</h5>
-  	<p>Toda contribuição será bem vinda</p>
-  	<a href="login.php" class="btn btn-light">Voltar Ao Topo</a>
   </div>
-  <div class="card-footer">
-  	Entrega do dia 18 de Setembro de 2019.
+  <div class="col-9">
+    
   </div>
-</footer>
-</body-->
+</div>
  <?php 
    include "foot.php"; 
  ?>
