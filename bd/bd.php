557718bc462ee82c06b7c9bc06e5328b84c9c08a
drkBiel <49046512+drkBiel@users.noreply.github.com>
2019-10-02 15:02:31 -0300

    Conclusão do Editar perfil
    Editar perfil, concluido, porém pode estar sujeito a alterações

--- bd/bd.php
+++ bd/bd.php
@@ -8,41 +8,61 @@
             } else {
                 return $conexao;
             }
-            
         }
 
+        // Ações do Usuário
         public function cadastrarUsuario($con, $nome, $email, $tipUsuario, $senha){
             $senha = md5($senha);
-            $contEmail = verificarEmail($email);
             
             $consulta  = "INSERT INTO `usuario`( `nome`, `email`, `tipoUsuario`, `senha`, `pontuacao`, `tempo_total`) VALUES ('$nome','$email','$tipUsuario','$senha',0,0)";
-
-            if ($contEmail > 1){
-                echo "<script type='javascript'>alert('Email ja cadastrado!');";
-            }else{
-                $resultado =  mysqli_query($con,$consulta);
-            }
- 
+            $resultado =  mysqli_query($con,$consulta);
+           
         }
 
-        public function selecionarUsuario($con, $email, $senha){
+        public function verificarUsuario($con, $email, $senha){
             $senha = md5($senha);
             $consulta = "SELECT nome, tipoUsuario from usuario WHERE email = '$email' and senha = '$senha'";
-            $resultado = mysqli_query($con, $consulta);
-            
-            return mysqli_fetch_array($resultado);
-            
-        }
-
-        private function verificarEmail($con, $email){
-            $senha = md5($senha);
-            $consulta = "SELECT tipoUsuario from usuario WHERE email = '$email'";
             $resultado = mysqli_query($con, $consulta);
             
             return mysqli_num_rows($resultado);
             
         }
-        
+
+        public function selecionarUsuario($con, $email){
+            $consulta = "SELECT * from usuario WHERE email = '$email'";
+            $resultado = mysqli_query($con, $consulta);
+            
+            $usuario = array();
+            $listUsuario = array();
+
+            $i = 0;
+
+            while ($listUsuarios = mysqli_fetch_array($resultado)){ 
+                $usuario[$i]['id'] = $listUsuarios['id_User'];
+                $usuario[$i]['nome'] = $listUsuarios['nome'];
+                $usuario[$i]['email'] = $listUsuarios['email'];
+    
+                $i++;        
+            }
+
+            return $usuario;
+        }
+
+
+        public function editarUsuario($con, $id, $nome, $email, $senha){
+            $senha = md5($senha);
+
+            if ($email == " "){
+                $comando = "UPDATE `usuario` SET nome='$nome', senha='$senha' WHERE id_User = $id";
+            }else if($senha == " "){
+                $comando = "UPDATE `usuario` SET nome='$nome', email='$email' WHERE id_User = $id";
+            }else{
+                $comando = "UPDATE `usuario` SET nome='$nome', email='$email', senha='$senha' WHERE id_User = $id";
+            }
+
+            $resultado = mysqli_query($con, $comando);
+                         
+        }
         
     }
 
