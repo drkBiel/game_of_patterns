ebb8edd49e69de4e5a7384ef5e7de60f4fcb4b18
Renan Lima <lima.renan96@gmail.com>
2019-10-08 09:25:42 -0300

    inicial

---
+++ view/inicial.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+      <title>Game Of Partterns</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="shortcut icon" href="../img/favicon.png">
+      <link rel="stylesheet" type="text/css" href="../estrutura/css/inicial.css">
+
+    </head>
+    <body>
+    <header> 
+    <div class="bg-gradient col-md-12">
+    <a href=""><img src="../img/logo.jpg" width="160" height="60"  class="position-absolute img-fluid text-hide"></a>
+    <ul class="nav justify-content-end form-inline">
+      <li class="nav-item">
+        <button type="button" class="btn btn-info m-2">
+          Página Inicial<span class="badge badge-gradient"><img src="../img/inicio-icone.png"></span>
+        </button> 
+
+        <button type="button" class="btn btn-info m-2">
+        Usuário <span class="badge badge-gradient"><img src="../img/perfil-icone.png"></span> 
+        </button>
+      </li>
+    </ul>
+    </div>
+    </header> 
+    <hr><!-- Barra -->
+    <h3 align="center">Teste seus conhecimentos sobre padrões de projetos - Quizzes</h3>
+    <div id="area_cards">
+      <div class="row"> <!-- Linha de cards -->
+          <div class="col-md-4">
+            <div class="card ml-4" style="width: 18rem;"> <!-- card -->
+                <div class="card-body">
+                  <h5 class="card-title">Padrões de Projeto I</h5>
+                  <p class="card-text">Teste seus conhecimentos sobre os padrões estruturais (Proxy, Facade e Adapter)</p>
+                  <a href="#" class="btn btn-warning">Iniciar</a>
+                  <a href="#" class="btn btn-info">Fórum</a>
+                </div>
+              </div>
+          </div>
+           <div class="col-md-4">
+            <div class="card" style="width: 18rem;"> <!-- card -->
+                <div class="card-body">
+                  <h5 class="card-title">Padrões de Projeto I</h5>
+                  <p class="card-text">Teste seus conhecimentos sobre os padrões estruturais (Proxy, Facade e Adapter)</p>
+                  <a href="#" class="btn btn-warning">Iniciar</a>
+                  <a href="#" class="btn btn-info">Fórum</a>
+                </div>
+              </div>
+          </div>
+           <div class="col-md-4">
+            <div class="card" style="width: 18rem;"> <!-- card -->
+                <div class="card-body">
+                  <h5 class="card-title">Padrões de Projeto I</h5>
+                  <p class="card-text">Teste seus conhecimentos sobre os padrões estruturais (Proxy, Facade e Adapter)</p>
+                  <a href="#" class="btn btn-warning">Iniciar</a>
+                  <a href="#" class="btn btn-info">Fórum</a>
+
+                </div>
+              </div>
+          </div>
+      </div>
+    </div>
+
+  
+
+    </body>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</html>
