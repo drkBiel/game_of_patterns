7fa9fd8591aed3ab19acbef0f22d7fb80c9f73f3
drkBiel <49046512+drkBiel@users.noreply.github.com>
2019-10-09 16:13:25 -0300

    Backend - Adicionado
    Foi adicionado o backend, ainda falta criar a função para selecionar as badges e listá-las.
    Adicionado uma barra de menu padrão.

--- view/perfil.php
+++ view/perfil.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
+     <?php 
+        require '../bd/bd.php';
+        $bd = new BD();
+        session_start();
+        $conexao = $bd->conexao();
+        $usuario = $bd->selecionarUsuario($conexao,$_SESSION['email']);
+      ?>
+      
       <title>Game Of Partterns</title>
 
       <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,52 +23,70 @@
     <a href=""><img src="../img/logo.jpg" width="160" height="60"  class="position-absolute img-fluid text-hide"></a>
     <ul class="nav justify-content-end form-inline">
       <li class="nav-item">
-        <button type="button" class="btn btn-info m-2">
-          Página Inicial<span class="badge badge-gradient"><img src="../img/inicio-icone.png"></span>
-        </button> 
+        <a href="inicial.php">
+            <button type="button" class="btn btn-info m-2">
+              Página Inicial<span class="badge badge-gradient"><img src="../img/inicio-icone.png"></span>
+            </button>
+          </a>
 
-        <button type="button" class="btn btn-info m-2">
-        Usuário <span class="badge badge-gradient"><img src="../img/perfil-icone.png"></span> 
-        </button>
+          <a href="../view/perfil.php">
+              <button type="button" class="btn btn-info m-2">
+                <?php echo $usuario[0]['nome'];?> <span class="badge badge-gradient"><img src="../img/perfil-icone.png"></span> 
+              </button>
+          </a>
       </li>
     </ul>
     </div>
     </header> 
     <hr><!-- Barra -->
-      <div class="container">
-       <div class="row">
-         <div align="center" class="col-md-3"> <!-- Imagem -->
-           <img src="../img/perfil.png">
-           <button type="submit" id="btnCad" class="btn btn-warning mt-2">
-            <span class="badge badge-gradient"><img src="../img/configuracao-icone.png"></span> Atualizar
-            </button>
+      <div class="container" style="margin-top: 5%;">
+        <div class="row">
+          <div class="col align-self-center"> <!-- Imagem -->
+              <img src="../img/perfil.png" style="margin-left: 13%;">
+              <a href="conf_perfil.php">
+                <button type="submit" id="btnCad" class="btn btn-warning mt-2">
+                  <span class="badge badge-gradient"><img src="../img/configuracao-icone.png"></span> Atualizar 
+                </button>
+              </a>
+            </div>
+
+          <div class="col-md-3"> <!-- Posição No Ranking -->
+            <b><h3 style="margin-top:16%; color: #fa7202; margin-left:9%;"> <?php echo $usuario[0]['nome'] ?> </h3></b> 
+            <div class="cd_perfil bg-light" style="margin-top:15%;">
+              <h5 align="center" id="tl_perfil">Posição no ranking</h5>
+              <h5 align="center" id="dd_perfil">4</h5>
+            </div>
+          </div>
+
+          <div class="col-md-3"> <!-- Posição geral -->
+            <div class="cd_perfil bg-light" style="margin-top:43%;">
+              <h5 align="center" id="tl_perfil">Pontuação geral</h5>
+              <h5 align="center" id="dd_perfil">4</h5>
+            </div>
+          </div>
+
+          <div class="col-md-3"> <!-- Numero de quizzes -->
+            <br>
+            <div class="cd_perfil bg-light" style="margin-top:33%;">
+              <h5 align="center" id="tl_perfil">Número de quizzes</h5>
+              <h5 align="center" id="dd_perfil">4</h5>
+            </div>
+          </div>
          </div>
-         <div class="col-md-3"> <!-- Posição No Ranking -->
-          <div class="cd_perfil bg-light">
-           <h6>Posição No Ranking</h6>
-           <span class="badge badge-gradient"><p>12</p></span>
-         </div>
-         </div> 
-         <div class="col-md-3"> <!-- Posição geral -->
-          <div class="cd_perfil bg-light">
-           <h6>Posição Geral</h6>
-           <span class="badge badge-gradient"><p>6</p></span>
-         </div>
-         </div>
-         <div class="col-md-3"> <!-- Numero de quizzes -->
-          <div class="cd_perfil bg-light">
-           <h6>Número de quizzes</h6>
-           <span class="badge badge-gradient"><p>4</p></span>
-         </div>
-         </div>
-         </div>
-       </div>
+        </div>
+
+        <div class="row" style="margin-left: 35%;">
+          <div class="col-md-3"> <!-- Posição No Ranking -->
+            <b><h3 style="margin-top:16%; color: #fa7202; margin-left:9%;"> Minhas badges </h3></b> 
+            <div class="cd_perfil bg-light" style="margin-top:15%;">
+              <h5 align="right" id="tl_perfil">  </h5>
+              <h5 align="center" id="dd_perfil"> </h5>
+            </div>
+          </div>
+
+        </div>
       </div>
-       
 
-
-
-    </body>
     <script src="../estrutura/js/jquery-3.4.1.js">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
