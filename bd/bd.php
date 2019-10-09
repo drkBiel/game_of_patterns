e90b5b6323707a89da27cdaf79fcbc7f16d5ab13
drkBiel <49046512+drkBiel@users.noreply.github.com>
2019-10-09 16:17:55 -0300

    Novas funções adicionadas
    Funcionais
           selecionarQuizzes
           selecionarQuestoes
           verificarResposta
    
    Não funcionais:
           selecionarIdBadges

--- bd/bd.php
+++ bd/bd.php
@@ -1,7 +1,9 @@
 <?php
     class BD{
         public function conexao(){
-            $conexao = mysqli_connect("us-cdbr-iron-east-05.cleardb.net","b067d1527549e8","b7cf4a61","heroku_74583cad15e5fd0");
+            mb_internal_encoding("UTF-8"); 
+            mb_http_output( "iso-8859-1" );  
+            $conexao = mysqli_connect("localhost","root","","db_gop");
             if (!$conexao) {
                 $men_erro = "Erro ao conectar" . mysqli_connect_error();
                 die($men_erro);
@@ -21,9 +23,9 @@
 
         public function verificarUsuario($con, $email, $senha){
             $senha = md5($senha);
-            $consulta = "SELECT nome, tipoUsuario from usuario WHERE email = '$email' and senha = '$senha'";
+            $consulta = "SELECT nome, tipo_Usuario from usuario WHERE email = '$email' and senha = '$senha'";
             $resultado = mysqli_query($con, $consulta);
-            
+            echo $consulta;
             return mysqli_num_rows($resultado);
             
         }
@@ -31,7 +33,6 @@
         public function selecionarUsuario($con, $email){
             $consulta = "SELECT * from usuario WHERE email = '$email'";
             $resultado = mysqli_query($con, $consulta);
-            
             $usuario = array();
             $listUsuario = array();
 
@@ -44,7 +45,6 @@
     
                 $i++;        
             }
-
             return $usuario;
         }
 
@@ -63,5 +63,76 @@
             $resultado = mysqli_query($con, $comando);
                          
         }
+
+        public function selecionarIdBadges($con, $email){
+            $consulta = "SELECT id'sBadges from usuario WHERE email = '$email'";
+            $resultado = mysqli_query($con, $consulta);
+            
+            $usuario = array();
+            $listUsuario = array();
+
+            $i = 0;
+
+            while ($listUsuarios = mysqli_fetch_array($resultado)){ 
+                $usuario[$i]['idBadges'] = $listUsuarios["id'sBadges"];
+                $i++;        
+            }
+
+            var_dump($usuario);
+
+            return $usuario;
+        }
+
+
+        // Funções do Quiz
+
+        public function selecionarQuizzes($con){
+            $comando =  "SELECT * FROM quiz";
+            $resultado = mysqli_query($con, $comando);
+            $tblQuiz = array();
+            $i = 0;
+            
+            while ($quizzes = mysqli_fetch_array($resultado)) {
+                $tblQuiz[$i]['id'] = $quizzes['id_Quiz'];
+                $tblQuiz[$i]['nome'] = $quizzes['nome'];
+                $tblQuiz[$i]['descricao'] = $quizzes['descricao'];
+
+                $i += 1;
+            }
+
+            return $tblQuiz;
+
+        }
+
+
+        public function selecionarQuestoes($con, $idQuiz){
+            $comando =  "SELECT * FROM perguntas WHERE id_Quiz = '$idQuiz'";
+            $resultado = mysqli_query($con, $comando);
+            $tblQuestoes = array();
+            $i = 0;
+
+           
+            while ($questoes = mysqli_fetch_array($resultado)) {
+                $tblQuestoes[$i]['idPerg'] = $questoes['id_Pergunta'];
+                $tblQuestoes[$i]['enunciado'] = $questoes['enunciado'];
+                $tblQuestoes[$i]['a'] = $questoes['alt_A'];
+                $tblQuestoes[$i]['b'] = $questoes['alt_B'];
+                $tblQuestoes[$i]['c'] = $questoes['alt_C'];
+                $tblQuestoes[$i]['d'] = $questoes['alt_D'];
+                $tblQuestoes[$i]['e'] = $questoes['alt_E'];
+
+                $i += 1;
+            }
+
+            return $tblQuestoes;
+        }
+
+        public function verificarResposta($con, $resposta, $idPergunta){
+            $comando =  "SELECT * FROM perguntas WHERE id_Pergunta = '$idPergunta' and alt_Correta = '$resposta'";
+            $resultado = mysqli_query($con, $comando);
+            echo $comando;
+            return mysqli_num_rows($resultado);
+        }
+
     }
-?>
+?> 
