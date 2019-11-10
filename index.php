638834d69693a00738d96834b5f59c39134d9cd3
Fabio Junior <falaprofabio@gmail.com>
2019-11-10 15:26:10 -0300

    correção de acentos no index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 
 <head>
-
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="estrutura/css/style.css">
