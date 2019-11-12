8ab92b73b386b23a011582e30c96c0f1b6073eae
drkBiel <49046512+drkBiel@users.noreply.github.com>
2019-11-11 21:27:35 -0300

    Correção de erro da tag script
    Tag <script> linha 120

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
           <input type="hidden" name="acao" value="logar">
 
           <div class="nav-item form-group">
-            <input class="btn btn-warning mr-2" type="submit" name="" value="Entrar e Sair">
+            <input class="btn btn-warning mr-2" type="submit" name="" value="Entrar">
           </div>
 
         </form>
@@ -117,7 +117,7 @@
   
 </body>
 
-<script src="estrutura/js/jquery-3.4.1.js">
+<script src="estrutura/js/jquery-3.4.1.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
