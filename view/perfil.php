89a8fc81ed389e851f5ab1cce76e8a8221f83276
Renan Lima <lima.renan96@gmail.com>
2019-11-26 11:00:53 -0300

    Update perfil.php

--- view/perfil.php
+++ view/perfil.php
@@ -60,16 +60,44 @@
             </div>
         </div>
 
-        <div style="margin-left: 30%;">
-            <div class="col-md-3"> <!-- Posição No Ranking -->
-                <b><h3 style="margin-top:16%; color: #fa7202; margin-left:9%;"> Minhas badges </h3></b> 
-                <div class="cd_perfil bg-light" style="margin-top:15%;">
-                    <h5 align="right" id="tl_perfil">  </h5>
-                    <h5 align="center" id="dd_perfil"> </h5>
+          <div class="container" style="margin-top: 5%;">
+            <div class="row">
+                <div class="col-md-3 align-self-center"> <!-- Imagem -->
+                    <img src="../img/perfil.png" style="margin-left: 13%;">
+                    <a href="conf_perfil.php">
+                        <button type="submit" id="btnCad" class="btn btn-warning mt-2">
+                            <span class="badge badge-gradient"><img src="../img/configuracao-icone.png"></span> Atualizar 
+                        </button>
+                    </a>
+                </div>
+
+                <div class="col-md-3"> <!-- Posição No Ranking -->
+                    <b><h3 style="margin-top:16%; color: #fa7202; margin-left:9%;"> <?php echo $usuario[0]['nome'] ?> </h3></b> 
+
+                    <div class="cd_perfil bg-light" style="margin-top:16%;">
+                        <h5 align="center" id="tl_perfil">Posição no ranking</h5>
+                        <h5 align="center" id="dd_perfil"><?php echo $posicao; ?></h5>
+                    </div>
+                </div>
+
+                <div class="col-md-3"> <!-- Posição geral -->
+                    <div class="cd_perfil bg-light" style="margin-top:16%;">
+                        <h5 align="center" id="tl_perfil">Pontuação geral</h5>
+                        <h5 align="center" id="dd_perfil"><?php echo $usuario[0]['pont'] ?></h5>
+                    </div>
+                </div>
+
+                <div class="col-md-3"> <!-- Numero de quizzes -->
+                    <div class="cd_perfil bg-light" style="margin-top:16%;">
+                        <h5 align="center" id="tl_perfil">Número de quizzes</h5>
+                        <h5 align="center" id="dd_perfil"> <?php echo count($hqr); ?> </h5>
+                    </div>
                 </div>
             </div>
-
         </div>
+
+
+        
    
 
     <script src="../estrutura/js/jquery-3.4.1.js"></script>
