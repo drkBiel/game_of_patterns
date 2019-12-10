0110a95f29dce4479270ff41d9f2c96e3dd1528c
Gabriel Alcântara <gabrielalcantara39@gmail.com>
2019-12-10 10:14:35 -0300

    Retirando data do rodapé

--- view/editarQuiz.php
+++ view/editarQuiz.php
@@ -215,7 +215,7 @@
 
         <footer class="card text-center">
             <div class="card-footer">
-                Entrega do dia 25 de Setembro de 2019.
+                <!--Entrega do dia 25 de Setembro de 2019.-->
             </div>
         </footer>
         
